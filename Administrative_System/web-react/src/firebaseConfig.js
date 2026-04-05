c0558eaa3023d3f10a169ba23e77a8158a463161
tbarckyasir <tbarckyasir@gmail.com>
2026-04-06 00:16:52 +0200

    Update UI + added ticket priority

--- Administrative_System/web-react/src/firebaseConfig.js
+++ Administrative_System/web-react/src/firebaseConfig.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCk7xXTF4lRn3v5i6F_leQvK-hdnFW_zcU",
   authDomain: "myproject-68794.firebaseapp.com",
@@ -13,10 +12,8 @@ const firebaseConfig = {
   appId: "1:252117852002:web:39eb53c016abfac4fe963b"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export the services so other files can use them
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export default app;
